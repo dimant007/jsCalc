ac62ca98c7fada6e4c424426c02c0fe7d8945424
dimant007 <ditryk@gmail.com>
2015-04-20 17:13:47 +0300

    fixes in design, making one display instead of two

--- index.js
+++ index.js
@@ -1,56 +1,30 @@
 /**
  * Created by ditry_000 on 20.04.2015.
  */
-var a, b;
-function setValues() {
-    a = Number(document.getElementById("a").value);
-    b = Number(document.getElementById("b").value);
-}
-function sum() {
-    setValues();
-    result = a + b;
-    alert("The sum is equals to " + result);
-}
-function rest() {
-    setValues();
-    result = a - b;
-    alert("The sum is equals to " + result);
-}
-function mult() {
-    setValues();
-    result = a * b;
-    alert("The sum is equals to " + result);
-}
-function div() {
-    setValues();
-    result = a / b;
-    alert("The sum is equals to " + result);
+function number(n){
+    document.form.display.value = document.form.display.value + n;
 }
 
-function num(n){
-    document.form.t1.value = document.form.t1.value + n;
+function acting(act){
+    oper = act;
+    num = document.form.display.value;
+    document.form.display.value = "";
 }
-function num(n){
-    document.form.t2.value = document.form.t2.value + n;
+function equal(){
+    calculating(eval(num), eval(document.form.display.value), oper);
+}
+function calculating(n1, n2, act){
+    if(act == "+"){
+        document.form.display.value = n1 + n2;
+    }
+    else if(act == "-"){
+        document.form.display.value = n1 - n2;
+    }
+    else if(act == "*"){
+        document.form.display.value = n1 * n2;
+    }
+    else if(act == "/"){
+        document.form.display.value = n1 / n2;
+    }
 }
 
-function operator(op){
-    oper = op;
-    num = document.form.t1.value;
-    document.form.t1.value = "";
-}
-
-function doesthejob(n1, n2, op){
-    if(op == "+"){
-        document.form.t1.value = n1 + n2;
-    }
-    else if(op == "-"){
-        document.form.t1.value = n1 - n2;
-    }
-    else if(op == "*"){
-        document.form.t1.value = n1 * n2;
-    }
-    else if(op == "/"){
-        document.form.t1.value = n1 / n2;
-    }
-}
